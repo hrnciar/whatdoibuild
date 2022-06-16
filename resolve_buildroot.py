21360939987509b11ae5824932ba908645f99714
Tomáš Hrnčiar <thrnciar@redhat.com>
2022-06-16 09:55:33 +0200

    progress

--- resolve_buildroot.py
+++ resolve_buildroot.py
@@ -89,6 +89,12 @@ def resolve_requires(requires, ignore_weak_deps=True):
     requires += tuple(mandatory_packages_in_groups())
     log(f'• Resolving {orig_len} requirements...', end=' ')
     for dep in requires:
+        if 'zope-component' in str(dep):  # XXX we have built an older version of zope-component, but this is broken on rawhide
+            continue
+        if 'python3-certbot ' in str(dep):  # certbot requires ^
+            continue
+        if 'python3dist(certbot)' in str(dep):  # certbot requires ^
+            continue
         selector = hawkey.Selector(sack).set(provides=dep)
         goal.install(select=selector)
     if not goal.run(ignore_weak_deps=ignore_weak_deps):
